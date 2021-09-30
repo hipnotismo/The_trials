3fde805ce80fc1e4562d3a09afceeaefba427ef5
martin <martinconcetti97@gmail.com>
2021-09-30 20:31:42 -0300

    Enemy prototipe and arrow movement

--- Da game/main.lua
+++ Da game/main.lua
@@ -1,11 +1,24 @@
 function love.load()
-x = 100
+    --player 
+    playerX = 10
+    playerY = 10
+    playerWith = 50
+    playerHight = 50
+    --enemies
+    enemy1X = 100
+    enemy1Y = 100
+    enemy1With =100
+    enemy1Hight = 100
 end
 
 function love.update(dt)
-    x = x + 5 * dt
+    if love.keyboard.isDown("right") then
+    playerX = playerX + 100 * dt
+end
 end
 
 function love.draw()
-    love.graphics.rectangle("line", x, 50, 200, 150)
+    love.graphics.rectangle("line", playerX, playerY, playerWith, playerHight)
+    love.graphics.rectangle("line", enemy1X, enemy1Y, enemy1With, enemy1Hight)
+
 end
