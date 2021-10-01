46fecdf94441beb0f1d7deec84300b805c80dee6
martin <martinconcetti97@gmail.com>
2021-10-01 00:00:25 -0300

    Full player movement and screen limits

--- Da game/main.lua
+++ Da game/main.lua
@@ -1,29 +1,69 @@
 function love.load()
     --screen control
-    screen = 0
+    screen = 1
+    --variables
+    speed = 500
+    width = 700
+    height = 500
+
+    playerPosX = 10
+    playerPosY = 100
+
     --player 
-    playerX = 10
-    playerY = 10
-    playerWith = 50
-    playerHight = 50
+   player = {
+        x = playerPosX,
+        y = playerPosY,
+        width = 100,
+        height = 100
+    }
+
     --enemies
-    enemy1X = 100
-    enemy1Y = 100
-    enemy1With =100
-    enemy1Hight = 100
+    enemy1 = {
+        x = 10,
+        y = 100,
+        width = 100,
+        height = 100
+    }
+
+   
 end
 
 function love.update(dt)
     if screen == 1 then
+        if player.x < width then
             if love.keyboard.isDown("right") then
-            playerX = playerX + 100 * dt
+            player.x = player.x + speed * dt
+            end
         end
+       
+        if player.x > 0 then
+         if love.keyboard.isDown("left") then
+            player.x = player.x - speed * dt
+            end
+        end
+
+        if player.y > 0 then
+            if love.keyboard.isDown("up") then
+            player.y = player.y - speed * dt
+            end
+        end
+
+        if player.y < height then
+            if love.keyboard.isDown("down") then
+            player.y = player.y + speed * dt
+            end
+        end
+        print(player.y)
+
+        
     end
 end
 
 function love.draw()
     if screen == 1 then
-        love.graphics.rectangle("line", playerX, playerY, playerWith, playerHight)
-        love.graphics.rectangle("line", enemy1X, enemy1Y, enemy1With, enemy1Hight)
+        love.graphics.setColor(255,0,0)
+        love.graphics.rectangle("fill", player.x, player.y, player.width, player.height)
+        love.graphics.setColor(0,255,0)
+        love.graphics.rectangle("line", enemy1.x, enemy1.y, enemy1.width, enemy1.height)
     end
 end
